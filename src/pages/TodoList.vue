45018d91e35853e16ad07a77ffc9cc7d7a848e94
pengsheng.zhu <1137846786@qq.com>
2021-08-24 17:12:10 +0800

    完成增删以及筛选

--- src/pages/TodoList.vue
+++ src/pages/TodoList.vue
@@ -31,13 +31,14 @@
             <input
               class="toggle"
               type="checkbox"
-              v-model="todoListFiltered[index].isCompleted"
+              v-model="item.isCompleted"
+              @click="onClickUpdateActiveNumber(index)"
             />
             <label class="todo_value">
               {{ item.title }}
               <span>{{ item.content }}</span>
-              <div class="edit">删除</div>
-              <div class="edit">编辑</div>
+              <div class="edit" @click="onClickRemoveTodo(index)">删除</div>
+              <div class="edit" @click="onClickEditShow()">编辑</div>
             </label>
           </div>
         </li>
@@ -46,19 +47,32 @@
     <footer class="footer">
       <ul class="filters">
         <li>
-          <a href="#/all">All</a>
+          <a href="#/all" @click="showAll()">All</a>
         </li>
         <li>
-          <a href="#/active">Uncompleted</a>
+          <a href="#/uncompleted" @click="showUncompleted()">Uncompleted</a>
         </li>
         <li>
-          <a href="#/completed">Completed</a>
+          <a href="#/completed" @click="showCompleted()">Completed</a>
         </li>
       </ul>
-      <button class="clear-completed">
+      <button class="clear-completed" @click="onClickRemoveAllCompleted()">
         Clear completed
       </button>
     </footer>
+    <section class="edit-popout" v-show="isEditShow">
+      <h2>编辑todo</h2>
+      <input type="text" v-model="thisTodoTitle" />
+      <br />
+      <input type="text" v-model="thisTodoContent" />
+      <br />
+      <select>
+        <option value="true">已完成</option>
+        <option value="false">未完成</option>
+      </select>
+      <br />
+      <button @click="onClickUpdateTodo()">确认</button>
+    </section>
   </section>
 </template>
 
@@ -70,6 +84,10 @@ export default {
       newTitle: "",
       newTodoContent: "",
       activeNumber: 0,
+      isEditShow: false,
+      thisTodoTitle: "",
+      thisTodoContent: "",
+      showModel: "All",
     };
   },
 
@@ -96,10 +114,78 @@ export default {
         document.querySelector(".new-todo-title").focus();
       }
     },
+
+    // 复选框 更新完成数量
+    onClickUpdateActiveNumber(index, event) {
+      // 筛选状态下不可编辑
+      event = event || window.event;
+      if (this.showModel != "All") {
+        event.preventDefault();
+      }
+      if (this.todoList[index].isCompleted === false) {
+        this.activeNumber--;
+      } else {
+        this.activeNumber++;
+      }
+    },
+
+    // 删除todo
+    onClickRemoveTodo(index) {
+      if (this.showModel === "All") {
+        if (this.todoList[index].isCompleted === false) {
+          this.activeNumber--;
+        }
+        this.todoList.splice(index, 1);
+      }
+    },
+
+    onClickEditShow() {
+      if (this.showModel === "All") {
+        this.isEditShow = true;
+      }
+    },
+
+    // 确认编辑
+    onClickUpdateTodo() {
+      this.isEditShow = false;
+      this.thisTodoTitle = "";
+      this.thisTodoContent = "";
+    },
+
+    // 批量删除已完成
+    onClickRemoveAllCompleted() {
+      for (let index = 0; index < this.todoList.length; index++) {
+        if (this.todoList[index].isCompleted === true) {
+          this.todoList.splice(index, 1);
+          index--;
+        }
+      }
+    },
+
+    showUncompleted() {
+      this.showModel = "Uncompleted";
+    },
+
+    showCompleted() {
+      this.showModel = "Completed";
+    },
+
+    showAll() {
+      this.showModel = "All";
+    },
   },
 
   computed: {
     todoListFiltered() {
+      if (this.showModel === "Uncompleted") {
+        return this.todoList.filter((item) => {
+          return item.isCompleted === false;
+        });
+      } else if (this.showModel === "Completed") {
+        return this.todoList.filter((item) => {
+          return item.isCompleted === true;
+        });
+      }
       return this.todoList;
     },
   },
